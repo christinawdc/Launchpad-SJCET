c67200eec5deaf0566ea3b79930e9f4c8da43acc
Jibin Gigi <jibingigi2026@cs.sjcetpalai.ac.in>
2025-06-12 16:23:25 +0530

    Initial setup '

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,20 @@
-import { useState } from "react";
-import "./App.css";
+import { Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import Profiles from "./pages/Profiles";
+import Projects from "./pages/Projects";
+import StartUps from "./pages/StartUps";
 
 function App() {
-  return <div></div>;
+  return (
+    <div>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/profiles" element={<Profiles />} />
+        <Route path="/projects" element={<Projects />} />
+        <Route path="/startups" element={<StartUps />} />
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
