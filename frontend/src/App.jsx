31c7b6284024fe7ffdc5e5377afc3861995c4c3b
Sona3105maria <sonamaria3105@gmail.com>
2025-06-19 11:07:47 +0530

    Connect page is created using react and shadcn

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,17 +3,22 @@ import Home from "./pages/Home";
 import Profiles from "./pages/Profiles";
 import Projects from "./pages/Projects";
 import StartUps from "./pages/StartUps";
+import "./index.css";
+import ConnectPage from "./pages/ConnectPage";
 
 function App() {
   return (
+    
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/profiles" element={<Profiles />} />
         <Route path="/projects" element={<Projects />} />
         <Route path="/startups" element={<StartUps />} />
+        <Route path="/connect" element={<ConnectPage />} />
       </Routes>
     </BrowserRouter>
+    
   );
 }
 
