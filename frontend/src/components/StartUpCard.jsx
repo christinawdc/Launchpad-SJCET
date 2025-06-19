96d3fd4dc3eb1600740f4cac704e21b9531ac400
christinawdc <christinawdc@gmail.com>
2025-06-19 18:36:28 +0530

    Updated project and startup card styles

--- frontend/src/components/StartUpCard.jsx
+++ frontend/src/components/StartUpCard.jsx
@@ -2,7 +2,10 @@ import React from "react";
 
 const StartUpCard = ({ logo = "LOGO" }) => {
   return (
-    <div className="bg-white rounded-3xl shadow-md w-full max-w-md mx-auto flex flex-col items-center p-3">
+    <div className="bg-white rounded-3xl shadow-md w-full max-w-md mx-auto flex flex-col items-center p-3"
+    style={{
+        border: "0.83px solid rgba(144, 146, 142, 0.5)"
+      }}>
       <div className="bg-[#E1FFD4] rounded-3xl w-full h-64 mx-2 mt-2"></div>
 
       <div className="w-full flex justify-center">
